7f33db5466c2fa918fb9b74c05c325793a69aad2
Cindy Chen <xchen@lcs.on.ca>
2024-01-10 12:20:40 -0500

    Initial Commit

---
+++ HomeApp/HomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppApp.swift
+//  HomeApp
+//
+//  Created by Cindy Chen on 2024-01-10.
+//
+
+import SwiftUI
+
+@main
+struct HomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
