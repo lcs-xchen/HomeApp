4246db475699303dc5273a0d4473ccc2658f913e
Cindy Chen <xchen@lcs.on.ca>
2024-01-12 09:59:04 -0500

    Completed the background gradient colors.

--- HomeApp/ContentView.swift
+++ HomeApp/ContentView.swift
@@ -8,17 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: Stored Properties
+    let firstGradientColors = Gradient(colors: [.lightBlueTop, .deepBlue, .deepPurple, .clear])
+    let secondGradientColors = Gradient(colors: [.deepYellow, .deepPurple, .clear])
+    
+    
+    //MARK: Computed Properties
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            //Background
+            Color.black
+            .ignoresSafeArea()
+            
+           //First Gradient
+            RadialGradient(
+                gradient: firstGradientColors,
+                center: .topLeading,
+                startRadius: 0,
+                endRadius: 1400
+            )
+            .ignoresSafeArea()
+            
+            //Second Gradient
+            RadialGradient(
+                gradient: secondGradientColors,
+                center: .bottomTrailing,
+                startRadius: 0,
+                endRadius: 1000
+            )
+            .ignoresSafeArea()
+     
+            
+            //Foreground
+            VStack {
+                
+                
+                
+                
+                
+                
+                
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
